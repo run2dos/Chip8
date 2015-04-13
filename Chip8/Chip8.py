30e76888a92c2a1f8c3fd543df1ce0aa6fe6569c
Kevin McGee(Mac Book Pro) <run2dos@gmail.com>
2015-04-12 22:45:19 -0700

    added a line to test the memory and stack.

--- Chip8/Chip8.py
+++ Chip8/Chip8.py
@@ -8,6 +8,7 @@ class Chip8:
         opcode = np.uint16
 
         self.memory = np.zeros([4096], dtype=np.uint8)
+        self.pc = 0x200
         self.reg_V = np.zeros(16, dtype=np.uint8)
 
         self.I = np.uint16(0x0)
@@ -23,12 +24,26 @@ class Chip8:
         self.display = np.zeros((64 * 32), dtype=np.uint8)
 
 
+    def run(self):
+        # fetch opcode
+        opcode = (self.memory[self.pc] << 8) | self.memory[self.pc + 1]
+        print(hex(opcode), self.pc)
+        # decode opcode
+            #execute opcode
+
 
 
 
 
 
 chip8 = Chip8()
+z = 0
+for i in range(len(chip8.memory)):
+    chip8.memory[z] = z
+    print(hex(chip8.memory[z]))
+    z += 1
 
-for x in chip8.stack:
-    print(x)
+chip8.run()
+
+#for x in chip8.stack:
+#    print(x)
